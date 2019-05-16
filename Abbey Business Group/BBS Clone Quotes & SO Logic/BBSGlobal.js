5d9df2263a1497cafc50e35496afd63338ef2e70
Cedric Griffiths <cedric.griffiths@brightbridgesolutions.com>
2019-05-16 12:08:03 +0100

    Move Abbey project

---
+++ Abbey Business Group/BBS Clone Quotes & SO Logic/BBSGlobal.js
@@ -0,0 +1,16 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       09 Feb 2017     cedricgriffiths
+ *
+ */
+function GlbCloneQuote() 
+{
+	LibCloneQuote();
+}
+
+function GlbPrintProject() 
+{
+	LibPrintProject();
+}
