0deedcbdbf5e6beea150ed2889b07a7857842125
Cedric Griffiths <cedric.griffiths@brightbridgesolutions.com>
2019-05-23 15:53:23 +0100

    bug fix

--- Recycling Technologies/BBS Create Inspection Records/BBSCreateInspectionRecordsUE.js
+++ Recycling Technologies/BBS Create Inspection Records/BBSCreateInspectionRecordsUE.js
@@ -24,89 +24,89 @@ function itemReceiptAS(type)
 			var currentRecord = nlapiGetNewRecord();
 			var recordId = currentRecord.getId();
 			var recordType = currentRecord.getRecordType();
-			var lines = currentRecord.getLineItemCount('item');
-			var supplierGRN = currentRecord.getFieldValue('custbody_bbs_purchase_supplier_grn');
-			var inspectionArray = {};
+			var itemReceiptRecord = null;
 			
-			for (var int = 1; int <= lines; int++) 
+			try 
 				{
-					var itemId = currentRecord.getLineItemValue('item', 'item', int);
-					var itemInspect = currentRecord.getLineItemValue('item', 'custcol_bbs_po_item_inspection_yn', int);
-					var itemLine = currentRecord.getLineItemValue('item', 'line', int);
-					var itemQuantity = currentRecord.getLineItemValue('item', 'quantity', int);
-					var itemRevision = currentRecord.getLineItemValue('item', 'custcol_bbs_po_required_revision', int);
+					itemReceiptRecord = nlapiLoadRecord(recordType, recordId);
+				} 
+			catch (error) 
+				{
+					itemReceiptRecord = null;
+				}
+			
+			if(itemReceiptRecord != null)
+				{
+					var lines = itemReceiptRecord.getLineItemCount('item');
+					var supplierGRN = itemReceiptRecord.getFieldValue('custbody_bbs_purchase_supplier_grn');
+					var inspectionArray = {};
 					
-					if(itemInspect == 'T')
+					for (var int = 1; int <= lines; int++) 
 						{
-							var inspectionRecord = null;
+							var itemId = itemReceiptRecord.getLineItemValue('item', 'item', int);
+							var itemInspect = itemReceiptRecord.getLineItemValue('item', 'custcol_bbs_po_item_inspection_yn', int);
+							var itemLine = itemReceiptRecord.getLineItemValue('item', 'line', int);
+							var itemQuantity = itemReceiptRecord.getLineItemValue('item', 'quantity', int);
+							var itemRevision = itemReceiptRecord.getLineItemValue('item', 'custcol_bbs_po_required_revision', int);
 							
-							try
+							if(itemInspect == 'T')
 								{
-									inspectionRecord = nlapiCreateRecord('customrecord_bbs_po_receipt_inspection');
-								}
-							catch(err)
-								{
-									inspectionRecord = null;
-								}
-							
-							if(inspectionRecord != null)
-								{
-									inspectionRecord.setFieldValue('custrecord_bbs_po_inspect_receipt_id', recordId);
-									inspectionRecord.setFieldValue('custrecord_bbs_po_inspect_item', itemId);
-									inspectionRecord.setFieldValue('custrecord_bbs_po_inspect_receipt', supplierGRN);
-									inspectionRecord.setFieldValue('custrecord_bbs_po_inspect_receipt_line', itemLine);
-									inspectionRecord.setFieldValue('custrecord_bbs_po_inspect_item_revision', itemRevision);
-									inspectionRecord.setFieldValue('custrecord_bbs_po_inspect_received_qty', itemQuantity);
-									
-									var inspectionId = null;
+									var inspectionRecord = null;
 									
 									try
 										{
-											inspectionId = nlapiSubmitRecord(inspectionRecord, true, true);
+											inspectionRecord = nlapiCreateRecord('customrecord_bbs_po_receipt_inspection');
 										}
 									catch(err)
 										{
-											inspectionId = null;
+											inspectionRecord = null;
 										}
 									
-									if(inspectionId != null)
+									if(inspectionRecord != null)
 										{
-											inspectionArray[itemLine] = inspectionId;
+											inspectionRecord.setFieldValue('custrecord_bbs_po_inspect_receipt_id', recordId);
+											inspectionRecord.setFieldValue('custrecord_bbs_po_inspect_item', itemId);
+											inspectionRecord.setFieldValue('custrecord_bbs_po_inspect_receipt', supplierGRN);
+											inspectionRecord.setFieldValue('custrecord_bbs_po_inspect_receipt_line', itemLine);
+											inspectionRecord.setFieldValue('custrecord_bbs_po_inspect_item_revision', itemRevision);
+											inspectionRecord.setFieldValue('custrecord_bbs_po_inspect_received_qty', itemQuantity);
+											
+											var inspectionId = null;
+											
+											try
+												{
+													inspectionId = nlapiSubmitRecord(inspectionRecord, true, true);
+												}
+											catch(err)
+												{
+													inspectionId = null;
+												}
+											
+											if(inspectionId != null)
+												{
+													inspectionArray[itemLine] = inspectionId;
+												}
 										}
 								}
 						}
-				}
-		
-			//Update the item receipt with the inspection records
-			//
-			if(Object.keys(inspectionArray).length > 0)
-				{
-					var itemReceiptRecord = null;
-					
-					try 
-						{
-							itemReceiptRecord = nlapiLoadRecord(recordType, recordId);
-						} 
-					catch (error) 
-						{
-							itemReceiptRecord = null;
-						}
-					
-					if(itemReceiptRecord != null)
+				
+					//Update the item receipt with the inspection records
+					//
+					if(Object.keys(inspectionArray).length > 0)
 						{
 							var receiptLines = itemReceiptRecord.getLineItemCount('item');
-							
+								
 							for (var int2 = 1; int2 <= receiptLines; int2++) 
 								{
 									var receiptLine = itemReceiptRecord.getLineItemValue('item', 'line', int2);
 									var inspectId = inspectionArray[receiptLine];
-									
+											
 									if(inspectId != null && inspectId != '' && inspectId != 'undefined')
 										{
 											itemReceiptRecord.setLineItemValue('item', 'custcol1', int2, inspectId);
 										}
 								}
-							
+									
 							nlapiSubmitRecord(itemReceiptRecord, false, true);
 						}
 				}
