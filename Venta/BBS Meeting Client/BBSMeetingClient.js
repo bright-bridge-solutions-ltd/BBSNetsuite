a6ebd6f5e7f83edc5502caeb205fec1850e0ab11
Cedric Griffiths <cedric.griffiths@brightbridgesolutions.com>
2019-06-21 16:33:42 +0100

    venta misc dev

---
+++ Venta/BBS Meeting Client/BBSMeetingClient.js
@@ -0,0 +1,24 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       21 Jun 2019     cedricgriffiths
+ *
+ */
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType 
+ * 
+ * @param {String} type Access mode: create, copy, edit
+ * @returns {Void}
+ */
+function meetingPageInit(type)
+{
+	//var a = '';
+	
+   //if(type == 'create')
+	//   {
+	//   		nlapiSetFieldValue('accesslevel', 'PUBLIC', true, true);
+	//   }
+}
