75264b00d20ae1f7e70e44a4017853d9ab9b4fcf
Cedric Griffiths <cedric.griffiths@brightbridgesolutions.com>
2019-07-04 16:29:46 +0100

    update time record with invoice details - move to scheduled script for large number of lines

--- BBS Internal System Dev/TimeInvoiceUE.js
+++ BBS Internal System Dev/TimeInvoiceUE.js
@@ -20,88 +20,86 @@
 function afterSumbitUpdateTimeTracking(type)
 {
     //get the current invoice record    
-    currentRecordId = nlapiGetRecordId();
-    var currentRec = nlapiLoadRecord('invoice',currentRecordId);
+    var currentRecordId = nlapiGetRecordId();
+    var currentRec = nlapiLoadRecord('invoice',currentRecordId); //10GU's
     
     // Get the number of line Time Tracking items submitted
     lines = currentRec.getLineItemCount('time'); 
     
-    //parse the list of time records
-    for ( var i=1; i<=lines; i++ )
-        {
-	        //get the ID of the Time Tracking 
-	        var timeRecId = currentRec.getLineItemValue('time', 'doc', i);
-	        var timeSelected = currentRec.getLineItemValue('time', 'apply', i);
-	        var timeQuantity = currentRec.getLineItemValue('time', 'quantity', i);
-	        var timeRate = currentRec.getLineItemValue('time', 'rate', i);
-	        var timeAmount = currentRec.getLineItemValue('time', 'amount', i);
-	        
-	        //if it's selected on the invoice, update its custom field
-	        if (timeSelected == 'T')
-	        	{
-	        		var timeRecord = null;
-	        		
-	        		try
-		        		{
-		        			timeRecord = nlapiLoadRecord('timebill', timeRecId);
-		        		}
-	        		catch(err)
-		        		{
-	        				timeRecord = null;
-		        		}
-	        		
-	        		if(timeRecord)
-	        			{
-		        			timeRecord.setFieldValue('custcol_bbs_related_invoice', currentRecordId);
-		        			timeRecord.setFieldValue('custcol_bbs_related_invoice_qty', timeQuantity);
-		        			timeRecord.setFieldValue('custcol_bbs_related_invoice_rate', timeRate);
-		        			timeRecord.setFieldValue('custcol_bbs_related_invoice_amt', timeAmount);
-		        			
-		        			try
+    //If we have a small number of lines then do the work here, else submit a job to do it
+    //
+    if(lines < 30)
+    	{
+		    //parse the list of time records
+		    for ( var i=1; i<=lines; i++ )
+		        {
+			        //get the ID of the Time Tracking 
+			        var timeRecId = currentRec.getLineItemValue('time', 'doc', i);
+			        var timeSelected = currentRec.getLineItemValue('time', 'apply', i);
+			        var timeQuantity = currentRec.getLineItemValue('time', 'quantity', i);
+			        var timeRate = currentRec.getLineItemValue('time', 'rate', i);
+			        var timeAmount = currentRec.getLineItemValue('time', 'amount', i);
+			        
+			        //if it's selected on the invoice, update its custom field
+			        if (timeSelected == 'T')
+			        	{
+			        		var timeRecord = null;
+			        		
+			        		try
+				        		{
+				        			timeRecord = nlapiLoadRecord('timebill', timeRecId); //10GU's
+				        		}
+			        		catch(err)
+				        		{
+			        				timeRecord = null;
+				        		}
+			        		
+			        		if(timeRecord)
 			        			{
-			        				nlapiSubmitRecord(timeRecord, false, true);
+				        			timeRecord.setFieldValue('custcol_bbs_related_invoice', currentRecordId);
+				        			timeRecord.setFieldValue('custcol_bbs_related_invoice_qty', timeQuantity);
+				        			timeRecord.setFieldValue('custcol_bbs_related_invoice_rate', timeRate);
+				        			timeRecord.setFieldValue('custcol_bbs_related_invoice_amt', timeAmount);
+				        			
+				        			try
+					        			{
+					        				nlapiSubmitRecord(timeRecord, false, true); //20GU's
+					        			}
+				        			catch(err)
+					        			{
+					        				nlapiLogExecution('ERROR', 'Error updating time bill record', err.message);
+					        			}
 			        			}
-		        			catch(err)
-			        			{
-			        				nlapiLogExecution('ERROR', 'Error updating time bill record', err.message);
-			        			}
-	        			}
-	        		
-	                 //nlapiSubmitField('timebill', timeRecId, 'custcol_bbs_related_invoice', currentRecordId );
-	                 //nlapiSubmitField('timebill', timeRecId, 'custcol_bbs_related_invoice_qty', timeQuantity );
-	                 //nlapiSubmitField('timebill', timeRecId, 'custcol_bbs_related_invoice_rate', timeRate );
-	                 //nlapiSubmitField('timebill', timeRecId, 'custcol_bbs_related_invoice_amt', timeAmount );
-	                 
-	        	}
-	        else
-	            {
-		            //ensure that updates on invoices when Time Tracking records are unapplied
-		            var timeRecord = nlapiLoadRecord('timebill', timeRecId);
-		            var invoiceNoSet = timeRecord.getFieldValue('custcol_bbs_related_invoice');
-		            
-		            if (invoiceNoSet != null)
-		            	{
-			            	timeRecord.setFieldValue('custcol_bbs_related_invoice', null);
-		        			timeRecord.setFieldValue('custcol_bbs_related_invoice_qty', null);
-		        			timeRecord.setFieldValue('custcol_bbs_related_invoice_rate', null);
-		        			timeRecord.setFieldValue('custcol_bbs_related_invoice_amt', null);
-		        			
-		        			try
-			        			{
-			        				nlapiSubmitRecord(timeRecord, false, true);
-			        			}
-		        			catch(err)
-			        			{
-			        				nlapiLogExecution('ERROR', 'Error updating time bill record', err.message);
-			        			}
-		            	
-		            	
-		                	//nlapiSubmitField('timebill', timeRecId, 'custcol_bbs_related_invoice', null ); 
-		                	//nlapiSubmitField('timebill', timeRecId, 'custcol_bbs_related_invoice_qty', null );
-			                //nlapiSubmitField('timebill', timeRecId, 'custcol_bbs_related_invoice_rate', null );
-			                //nlapiSubmitField('timebill', timeRecId, 'custcol_bbs_related_invoice_amt', null );
-		            	}
-	            }
-        }
+			        	}
+			        else
+			            {
+				            //ensure that updates on invoices when Time Tracking records are unapplied
+				            var timeRecord = nlapiLoadRecord('timebill', timeRecId);
+				            var invoiceNoSet = timeRecord.getFieldValue('custcol_bbs_related_invoice');
+				            
+				            if (invoiceNoSet != null)
+				            	{
+					            	timeRecord.setFieldValue('custcol_bbs_related_invoice', null);
+				        			timeRecord.setFieldValue('custcol_bbs_related_invoice_qty', null);
+				        			timeRecord.setFieldValue('custcol_bbs_related_invoice_rate', null);
+				        			timeRecord.setFieldValue('custcol_bbs_related_invoice_amt', null);
+				        			
+				        			try
+					        			{
+					        				nlapiSubmitRecord(timeRecord, false, true);
+					        			}
+				        			catch(err)
+					        			{
+					        				nlapiLogExecution('ERROR', 'Error updating time bill record', err.message);
+					        			}
+				            	}
+			            }
+		        }
+    	}
+    else
+    	{
+	    	var scheduleParams = {custscript_invoice_id: currentRecordId};
+	    	nlapiScheduleScript('customscript_invoice_scheduled', null, scheduleParams);
+    	}
 } 
        
