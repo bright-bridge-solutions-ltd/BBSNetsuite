2f5a2b641f2f2e609f613bfea8da2bfa2b78da3a
sambatten <sambatten@10.10.0.44>
2019-07-17 10:42:34 +0100

    Initial Version

--- Bannatyne/BBS Inventory Count/BBSInvCountClient.js
+++ Bannatyne/BBS Inventory Count/BBSInvCountClient.js
@@ -8,6 +8,13 @@
 
 function pageInit(type)
 	{
-		// disable the rate column
-		nlapiDisableLineItemField('item', 'rate', true);
+		// get the ID of the current user's role
+		var userRole = nlapiGetRole();
+		
+		// if statement to check the the user's role is not Administrator (ID 3) or TBG - Procurement team (ID 1028) or TBG - Stock Controller (ID 1029)
+		if (userRole != 3 && userRole != 1028 && userRole != 1029)
+			{
+				// disable the rate column
+				nlapiDisableLineItemField('item', 'rate', true);
+			}
 	}
