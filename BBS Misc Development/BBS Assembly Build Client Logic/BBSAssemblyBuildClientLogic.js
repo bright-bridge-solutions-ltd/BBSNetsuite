47d9e2358d4cd845ca95245243efbde29dbc557c
Cedric Griffiths <cedric.griffiths@brightbridgesolutions.com>
2019-09-10 17:06:39 +0100

    andy demo

---
+++ BBS Misc Development/BBS Assembly Build Client Logic/BBSAssemblyBuildClientLogic.js
@@ -0,0 +1,18 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       10 Sep 2019     cedricgriffiths
+ *
+ */
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ *   
+ * @returns {Boolean} True to continue save, false to abort save
+ */
+function clientSaveRecord(){
+
+    return true;
+}
