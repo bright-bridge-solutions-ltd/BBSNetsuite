4b413f1b04995fd86cb6a004d340d5442a13d946
sambatten <sambatten@10.10.0.46>
2019-09-10 16:21:30 +0100

    Initial Version

---
+++ Onfido V2.0 api/BBS Contract Logic/BBSContractClient.js
@@ -0,0 +1,211 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define(['N/format'],
+function(format) {
+    
+    /**
+     * Function to be executed after page is initialized.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
+     *
+     * @since 2015.2
+     */
+    function pageInit(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @since 2015.2
+     */
+    function fieldChanged(scriptContext) {
+    	
+    	// get the current record object
+    	var currentRecord = scriptContext.currentRecord;
+    	
+    	// get the field that was changed
+    	var fieldId = scriptContext.fieldId;
+    	
+    	// if statement to check that the start date field has been changed
+    	if (fieldId == 'custrecord_bbs_contract_start_date')
+    		{
+	    		// get the value of the start date field
+				var startDate = currentRecord.getValue({
+					fieldId: 'custrecord_bbs_contract_start_date'
+				});
+    		
+    			// get the value of the contract term field
+    			var contractTerm = currentRecord.getValue({
+    				fieldId: 'custrecord_bbs_contract_term'
+    			});
+    			
+    			// if statement to check that the user has selected a contractTerm and a start date
+    			if (contractTerm && startDate)
+    				{
+		    			contractTerm = parseInt(contractTerm); // convert to integer number
+		    			
+		    			// construct a new date object and set the startDate as it's value
+		    			startDate = new Date(startDate);
+		    			
+		    			// get the day from the date object
+		    			var day = startDate.getDate();
+		    			console.log(day);
+		    			
+		    			// if statement to check if the day variable is equal to 1
+		    			if (day == 1)
+		    				{
+		    					// increase the contract term variable by 1
+		    					contractTerm++;
+		    				}
+		    			else // day variable returns a value greater than 1
+		    				{
+		    					// increase the contractTerm variable by 2
+		    					contractTerm = contractTerm+2;
+		    				}
+		    			
+						// create a new date object and set it's value. This will be the last day of the month
+						var endDate = new Date(startDate.getFullYear(), startDate.getMonth()+contractTerm, 0);
+		    			
+		    			// set the end date field on the record
+		    			currentRecord.setValue({
+		    				fieldId: 'custrecord_bbs_contract_end_date',
+		    				value: endDate
+		    			});	
+		    		}
+    		}
+    }
+
+    /**
+     * Function to be executed when field is slaved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     *
+     * @since 2015.2
+     */
+    function postSourcing(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed after sublist is inserted, removed, or edited.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function sublistChanged(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed after line is selected.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function lineInit(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @returns {boolean} Return true if field is valid
+     *
+     * @since 2015.2
+     */
+    function validateField(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when sublist line is committed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateLine(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when sublist line is inserted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateInsert(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is deleted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateDelete(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is saved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @returns {boolean} Return true if record is valid
+     *
+     * @since 2015.2
+     */
+    function saveRecord(scriptContext) {
+
+    }
+
+    return {
+        fieldChanged: fieldChanged
+    };
+    
+});
---
+++ Venta (v2.0 API)/BBS Free Gift UE/BBSFreeGiftUE.js
@@ -0,0 +1,113 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType UserEventScript
+ * @NModuleScope SameAccount
+ */
+define(['N/runtime', 'N/search'],
+function(runtime, search) {
+   
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {string} scriptContext.type - Trigger type
+     * @param {Form} scriptContext.form - Current form
+     * @Since 2015.2
+     */
+    function beforeLoad(scriptContext) {
+
+    }
+
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {Record} scriptContext.oldRecord - Old record
+     * @param {string} scriptContext.type - Trigger type
+     * @Since 2015.2
+     */
+    function beforeSubmit(scriptContext) {
+    	
+    	// get the current record
+    	var currentRecord = scriptContext.newRecord;
+    	
+    	// get the current script
+    	var currentScript = runtime.getCurrentScript();
+    	
+    	// retrieve script parameters
+	    var orderValue = currentScript.getParameter({
+	    	name: 'custscript_free_items_ue_order_value'
+	    });
+	    
+	    // get the internal ID of the customer from the current record
+	    var customerID = currentRecord.getValue({
+	    	fieldId: 'entity'
+	    });
+	    
+	    // lookup fields on the customer record
+	    var customerLookup = search.lookupFields({
+	    	type: search.Type.CUSTOMER,
+			id: customerID,
+			columns: ['custentity_bb_want_fudge']
+		});
+	    
+	    // get value of 'wants free gift' checkbox from the customer lookup
+	    var freeGift = customerLookup.custentity_bb_want_fudge;
+	    
+	    log.debug({
+	    	title: 'Free Gift',
+	    	details: freeGift
+	    });
+	    
+	    // get the sales order total from the current record
+		var total = currentRecord.getValue({
+			fieldId: 'total'
+		});
+		
+		log.debug({
+			title: 'SO Total',
+			details: total
+		});
+		
+		// check if the freeGift variable returns true and the total variable is greater than 250
+		if (freeGift == true && total >= 250)
+			{
+				// tick the 'Wants Free Gift' checkbox
+				currentRecord.setValue({
+					fieldId: 'custbody_bb_want_fudge',
+					value: true
+				});
+			}
+		else if (total < 250) // if the total variable is less than 250
+			{
+				// untick the 'Wants Free Gift' checkbox
+				currentRecord.setValue({
+					fieldId: 'custbody_bb_want_fudge',
+					value: false
+				});
+			}
+
+    }
+
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {Record} scriptContext.oldRecord - Old record
+     * @param {string} scriptContext.type - Trigger type
+     * @Since 2015.2
+     */
+    function afterSubmit(scriptContext) {
+
+    }
+
+    return {
+        beforeLoad: beforeLoad,
+        beforeSubmit: beforeSubmit,
+        afterSubmit: afterSubmit
+    };
+    
+});
