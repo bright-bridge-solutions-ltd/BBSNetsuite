bf28c9df7c6cf7fbb5bb782a171bbedf7d4b9248
sambatten <sambatten@10.10.0.46>
2019-09-16 15:53:31 +0100

    added if statement to only reset description/unit price/quantity if the item has been changed

--- Recycling Technologies (v2.0 API)/BBS Purchase Order UE/BBSPurchaseOrderUE.js
+++ Recycling Technologies (v2.0 API)/BBS Purchase Order UE/BBSPurchaseOrderUE.js
@@ -23,6 +23,8 @@ function() {
     	var quantity;
     	var oldLineNo;
     	var newLineNo;
+    	var oldItem;
+    	var newItem;
 
     	// get the old and new record images
     	var oldRecord = scriptContext.oldRecord;
@@ -52,48 +54,64 @@ function() {
     			// check that the oldLineNo and newLineNo variables are the same
     			if (oldLineNo == newLineNo)
     				{
-
-			    		// get line fields from the old record image
-			    		description = oldRecord.getSublistValue({
-			    			sublistId: 'item',
-			    			fieldId: 'description',
-			    			line: x
-			    		});
-    			
-		    			unitPrice = oldRecord.getSublistValue({
-		    				sublistId: 'item',
-		    				fieldId: 'rate',
-		    				line: x
-		    			});
-    			
-		    			quantity = oldRecord.getSublistValue({
-		    				sublistId: 'item',
-		    				fieldId: 'quantity',
-		    				line: x
-		    			});
-    			
-		    			// set line fields on the new record image using values from the old record image
-		    			newRecord.setSublistValue({
-		    				sublistId: 'item',
-		    				fieldId: 'description',
-		    				value: description,
-		    				line: x
-		    			});
-    			
-		    			newRecord.setSublistValue({
-		    				sublistId: 'item',
-		    				fieldId: 'rate',
-		    				value: unitPrice,
-		    				line: x
-		    			});
-    			
-		    			newRecord.setSublistValue({
-		    				sublistId: 'item',
-		    				fieldId: 'quantity',
-		    				value: quantity,
-		    				line: x
-		    			});
-
+    					// get the item from the old and new record images
+    					oldItem = oldRecord.getSublistValue({
+    						sublistId: 'item',
+    						fieldId: 'item',
+    						line: x
+    					});
+    					
+    					newItem = newRecord.getSublistValue({
+    						sublistId: 'item',
+    						fieldId: 'item',
+    						line: x
+    					});
+    					
+    					// check that the oldItem and newItem variables are NOT the same
+    					if (oldItem != newItem)
+    						{
+					    		// get line fields from the old record image
+					    		description = oldRecord.getSublistValue({
+					    			sublistId: 'item',
+					    			fieldId: 'description',
+					    			line: x
+					    		});
+		    			
+				    			unitPrice = oldRecord.getSublistValue({
+				    				sublistId: 'item',
+				    				fieldId: 'rate',
+				    				line: x
+				    			});
+		    			
+				    			quantity = oldRecord.getSublistValue({
+				    				sublistId: 'item',
+				    				fieldId: 'quantity',
+				    				line: x
+				    			});
+		    			
+				    			// set line fields on the new record image using values from the old record image
+				    			newRecord.setSublistValue({
+				    				sublistId: 'item',
+				    				fieldId: 'description',
+				    				value: description,
+				    				line: x
+				    			});
+		    			
+				    			newRecord.setSublistValue({
+				    				sublistId: 'item',
+				    				fieldId: 'rate',
+				    				value: unitPrice,
+				    				line: x
+				    			});
+		    			
+				    			newRecord.setSublistValue({
+				    				sublistId: 'item',
+				    				fieldId: 'quantity',
+				    				value: quantity,
+				    				line: x
+				    			});
+				    			
+    						}
     				}
 			}
     }
