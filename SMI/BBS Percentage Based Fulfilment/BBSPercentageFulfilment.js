8f8aa6995de7d518583e0e14c016d3adae125bc3
Cedric Griffiths <cedric.griffiths@brightbridgesolutions.com>
2019-10-22 16:49:41 +0100

    bug fix

--- SMI/BBS Percentage Based Fulfilment/BBSPercentageFulfilment.js
+++ SMI/BBS Percentage Based Fulfilment/BBSPercentageFulfilment.js
@@ -23,56 +23,75 @@ function percentageFulfilmentARS(type)
 		{
 			var newRecord = nlapiGetNewRecord();
 			var newId = newRecord.getId();
-			var soRecord = nlapiLoadRecord('salesorder', newId);
-			var soLines = soRecord.getLineItemCount('item');
-			var soStatus = soRecord.getFieldValue('status');
-			
-			var qtyOrdered = Number(0);
-			var qtyCommitted = Number(0);
-			var qtyFulfilled = Number(0);
-			var percent = Number(0);
-			
-			for (var int = 1; int <= soLines; int++) 
-			{
-				var itemType = nlapiGetLineItemValue('item', 'itemtype', int);
-				
-				if(itemType == 'InvtPart' || itemType == 'Assembly')
-					{
-						qtyOrdered += Number(nlapiGetLineItemValue('item', 'quantity', int));
-						qtyCommitted += Number(nlapiGetLineItemValue('item', 'quantitycommitted', int));
-						qtyFulfilled += Number(nlapiGetLineItemValue('item', 'quantityfulfilled', int));
-					}
-			}
-			
-			if((qtyOrdered - qtyFulfilled) > 0)
+			var soRecord = null;
+			try
 				{
-					percent = Number(((qtyCommitted / (qtyOrdered - qtyFulfilled)) * 100.00).toFixed());
+					soRecord = nlapiLoadRecord('salesorder', newId);
+				}
+			catch(err)
+				{
+					soRecord = null;
 				}
 			
-			var percentListValue = null;
-			
-			if (percent >= 0 && percent <= 49)
-			{
-				percentListValue = 1;
-			}
-		
-			if (percent >= 50 && percent <= 74)
-			{
-				percentListValue = 2;
-			}
-		
-			if (percent >= 75 && percent <= 99)
-			{
-				percentListValue = 3;
-			}
-		
-			if (percent == 100)
-			{
-				percentListValue = 4;
-			}
-		
-			soRecord.setFieldValue('custbody_bbs_fulfilment_percentage', percentListValue);
-			
-			nlapiSubmitRecord(soRecord, false, true);
+			if(soRecord != null)
+				{
+					var soLines = soRecord.getLineItemCount('item');
+					var soStatus = soRecord.getFieldValue('status');
+					
+					var qtyOrdered = Number(0);
+					var qtyCommitted = Number(0);
+					var qtyFulfilled = Number(0);
+					var percent = Number(0);
+					
+					for (var int = 1; int <= soLines; int++) 
+						{
+							var itemType = nlapiGetLineItemValue('item', 'itemtype', int);
+							
+							if(itemType == 'InvtPart' || itemType == 'Assembly')
+								{
+									qtyOrdered += Number(nlapiGetLineItemValue('item', 'quantity', int));
+									qtyCommitted += Number(nlapiGetLineItemValue('item', 'quantitycommitted', int));
+									qtyFulfilled += Number(nlapiGetLineItemValue('item', 'quantityfulfilled', int));
+								}
+						}
+					
+					if((qtyOrdered - qtyFulfilled) > 0)
+						{
+							percent = Number(((qtyCommitted / (qtyOrdered - qtyFulfilled)) * 100.00).toFixed());
+						}
+					
+					var percentListValue = null;
+					
+					if (percent >= 0 && percent <= 49)
+						{
+							percentListValue = 1;
+						}
+				
+					if (percent >= 50 && percent <= 74)
+						{
+							percentListValue = 2;
+						}
+				
+					if (percent >= 75 && percent <= 99)
+						{
+							percentListValue = 3;
+						}
+				
+					if (percent == 100)
+						{
+							percentListValue = 4;
+						}
+				
+					try
+						{
+							soRecord.setFieldValue('custbody_bbs_fulfilment_percentage', percentListValue);
+							nlapiSubmitRecord(soRecord, false, true);
+						}
+					catch(err)
+						{
+						
+						}
+					
+				}
 		}
 }
