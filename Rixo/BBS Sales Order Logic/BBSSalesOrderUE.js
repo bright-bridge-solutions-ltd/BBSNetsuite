95cb6abf78eecf8b8e6b3ecfcb036fbd112a72ae
Sam Batten <sam.batten@brightbridgesolutions.com>
2020-07-22 17:27:32 +0100

    commit 22 Jul 2020

--- Rixo/BBS Sales Order Logic/BBSSalesOrderUE.js
+++ Rixo/BBS Sales Order Logic/BBSSalesOrderUE.js
@@ -12,6 +12,60 @@ function(runtime, record, search, file, xml)
 {
 	function salesOrderBeforeSubmit(scriptContext) {
 			
+			// check the record is being created or edited
+			if (scriptContext.type == scriptContext.UserEventType.CREATE || scriptContext.type == scriptContext.UserEventType.EDIT)
+				{
+					// retrieve script parameters
+					var splitLevelAmount = runtime.getCurrentScript().getParameter({
+						name: 'custscript_bbs_split_ship_indicator_amt'
+					});
+					
+					// declare and initialize variables
+					var lineTotal = 0;
+					var splitShipIndicator = 1;
+					
+					// get the current record
+					var currentRecord = scriptContext.newRecord;
+					
+					// get line item count
+					var lineCount = currentRecord.getLineCount({
+						sublistId: 'item'
+					});
+					
+					// loop through items
+					for (var i = 0; i < lineCount; i++)
+						{
+							// get the line total
+							var lineAmount = currentRecord.getSublistValue({
+								sublistId: 'item',
+								fieldId: 'amount',
+								line: i
+							});
+							
+							// add lineAmount to the lineTotal
+							lineTotal += lineAmount;
+							
+							// check if lineTotal is greater than splitLevelAmount script parameter AND this is not the first line
+							if (lineTotal > splitLevelAmount && i > 0)
+								{
+									// increase splitShipIndicator variable by 1
+									splitShipIndicator++;
+									
+									// set lineTotal variable to be the lineAmount
+									lineTotal = lineAmount;
+								}
+							
+							// set the Split Ship field using the splitShip variable
+							currentRecord.setSublistValue({
+								sublistId: 'item',
+								fieldId: 'custcol_bbs_split_ship',
+								value: splitShipIndicator,
+								line: i
+							});
+							
+						}
+				}
+		
 			// check the record is being created and the excecution context is web services
 			if (scriptContext.type == scriptContext.UserEventType.CREATE && runtime.executionContext == runtime.ContextType.WEBSERVICES)
 				{
