cb597809e803b56ebfb111af027806d28b08788b
Sam Batten <sam.batten@brightbridgesolutions.com>
2020-07-31 09:34:04 +0100

    commit 31 Jul 2020

---
+++ Astleys (v2.0 API)/BBS Customer Logic/BBSCustomerCL.js
@@ -0,0 +1,202 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define(['N/ui/dialog'],
+function(dialog) {
+    
+    /**
+     * Function to be executed after page is initialized.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
+     *
+     * @since 2015.2
+     */
+    function pageInit(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @since 2015.2
+     */
+    function fieldChanged(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed when field is slaved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     *
+     * @since 2015.2
+     */
+    function postSourcing(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed after sublist is inserted, removed, or edited.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function sublistChanged(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed after line is selected.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function lineInit(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @returns {boolean} Return true if field is valid
+     *
+     * @since 2015.2
+     */
+    function validateField(scriptContext) {
+    	
+    	// if the custentity_bbs_customer_sales_note has been changed
+    	if (scriptContext.fieldId == 'custentity_bbs_customer_sales_note')
+    		{
+    			// get the length of the custentity_bbs_customer_sales_note field
+    			var fieldLength = scriptContext.currentRecord.getValue({
+    				fieldId: 'custentity_bbs_customer_sales_note'
+    			}).length;
+    			
+    			// if fieldLength > 250
+    			if (fieldLength > 250)
+    				{
+    					// display an error message
+    					dialog.alert({
+    						title: '⚠️ Error',
+    						message: 'You have exceeded the maximum length for the Customer Document Note field of <b>250</b> characters.<br><br>Please amend this field before saving the record.'
+    					});
+    				}
+    		}
+    	else
+    		{
+    			return true;
+    		}
+
+    }
+
+    /**
+     * Validation function to be executed when sublist line is committed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateLine(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when sublist line is inserted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateInsert(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is deleted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateDelete(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is saved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @returns {boolean} Return true if record is valid
+     *
+     * @since 2015.2
+     */
+    function saveRecord(scriptContext) {
+    	
+    	// get the length of the custentity_bbs_customer_sales_note field
+		var fieldLength = scriptContext.currentRecord.getValue({
+			fieldId: 'custentity_bbs_customer_sales_note'
+		}).length;
+		
+		// if fieldLength > 250
+		if (fieldLength > 250)
+			{
+				// display an error message
+				dialog.alert({
+					title: '⚠️ Error',
+					message: 'You have exceeded the maximum length for the Customer Document Note field of <b>250</b> characters.<br><br>Please amend this field before saving the record.'
+				});
+			}
+		else
+			{
+				// allow the record to be saved
+				return true;
+			}
+
+    }
+
+    return {
+        validateField: validateField,
+        saveRecord: saveRecord
+    };
+    
+});
