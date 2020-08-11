746bad332d9631d35d8a77e666f255f28c0b38b1
Cedric Griffiths <cedric.griffiths@brightbridgesolutions.com>
2020-08-11 17:16:55 +0100

    dev 11/08/20

--- Rixo/BBS GL Plugin/BBSSamplesGlPlugin.js
+++ Rixo/BBS GL Plugin/BBSSamplesGlPlugin.js
@@ -13,9 +13,8 @@ function customizeGlImpact(transactionRecord, standardLines, customLines, book)
 			//Get the parameters
 			//
 			var context 			= nlapiGetContext();
-			var samplesLocation 	= context.getSetting('SCRIPT', 'custscript_bbs_sample_location');
-			var samplesCogs 		= context.getSetting('SCRIPT', 'custscript_bbs_sample_cogs');
-			var standardCogs 		= context.getSetting('SCRIPT', 'custscript_bbs_standard_cogs');
+			var samplesLocation 	= 315;
+			var standardCogs 		= 340;
 			
 			//Get the source record type & id
 			//
@@ -24,55 +23,62 @@ function customizeGlImpact(transactionRecord, standardLines, customLines, book)
 			
 			if(recType == 'itemfulfillment')
 				{
-					//Get the count of standard lines
+					//Get the destination COGS account
 					//
-					var linecount = standardLines.getCount();
-													
-					//Loop round the lines
-					//
-					if(linecount > 0)
-						{								
-							//Loop through the standard GL lines
-							//			
-							for (var i=0; i<linecount; i++) 
-								{
-									//Get the line object
-									//
-									var line = standardLines.getLine(i);
-																					
-									//Ignore the summary line or any non-posting lines
-									//
-									if(line.isPosting() && line.getId() != 0)
-										{
-											var account 	= line.getAccountId();
-											var debit 		= Number(line.getDebitAmount());
-											var entityId 	= line.getEntityId();
-											var location 	= line.getLocationId();
-												
-											//Find the relevant posting line by looking at the account id
-											//
-											if(samplesLocation == location && account == standardCogs)
-												{
-													//Add new posting lines here
-													//
-													if(debit != 0)
-														{
-															//Credit the original line
-															//
-															var newLine = customLines.addNewLine();
-															newLine.setAccountId(parseInt(standardCogs));
-															newLine.setCreditAmount(debit);
-															newLine.setMemo('Custom GL Plugin Samples Posting');
-															newLine.setEntityId(entityId);
+					var samplesCogs = transactionRecord.getFieldValue('custbody_bbs_press_cogs');
+					
+					if(samplesCogs != null && samplesCogs != '')
+						{
+							//Get the count of standard lines
+							//
+							var linecount = standardLines.getCount();
 															
-															//Debit the "Samples COGS" account
+							//Loop round the lines
+							//
+							if(linecount > 0)
+								{								
+									//Loop through the standard GL lines
+									//			
+									for (var i=0; i<linecount; i++) 
+										{
+											//Get the line object
+											//
+											var line = standardLines.getLine(i);
+																							
+											//Ignore the summary line or any non-posting lines
+											//
+											if(line.isPosting() && line.getId() != 0)
+												{
+													var account 	= line.getAccountId();
+													var debit 		= Number(line.getDebitAmount());
+													var entityId 	= line.getEntityId();
+													var location 	= line.getLocationId();
+														
+													//Find the relevant posting line by looking at the account id
+													//
+													if(samplesLocation == location && account == standardCogs)
+														{
+															//Add new posting lines here
 															//
-															var newLine = customLines.addNewLine();
-															newLine.setAccountId(parseInt(samplesCogs));
-															newLine.setDebitAmount(debit);
-															newLine.setMemo('Custom GL Plugin Samples Posting');
-															newLine.setEntityId(entityId);
-														}							
+															if(debit != 0)
+																{
+																	//Credit the original line
+																	//
+																	var newLine = customLines.addNewLine();
+																	newLine.setAccountId(parseInt(standardCogs));
+																	newLine.setCreditAmount(debit);
+																	newLine.setMemo('Custom GL Plugin Samples Posting');
+																	newLine.setEntityId(entityId);
+																	
+																	//Debit the "Samples COGS" account
+																	//
+																	var newLine = customLines.addNewLine();
+																	newLine.setAccountId(parseInt(samplesCogs));
+																	newLine.setDebitAmount(debit);
+																	newLine.setMemo('Custom GL Plugin Samples Posting');
+																	newLine.setEntityId(entityId);
+																}							
+														}
 												}
 										}
 								}
