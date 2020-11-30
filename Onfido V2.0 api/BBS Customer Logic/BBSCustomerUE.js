13cb71b5d238ee40eca861bf6741d9e631c398b2
Sam Batten <sam.batten@brightbridgesolutions.com>
2020-11-30 16:26:51 +0000

    Dev 30/11/2020

--- Onfido V2.0 api/BBS Customer Logic/BBSCustomerUE.js
+++ Onfido V2.0 api/BBS Customer Logic/BBSCustomerUE.js
@@ -45,12 +45,8 @@ function(record, search) {
     function afterSubmit(scriptContext) {
     	
     	// only run script when the record is being created or edited
-    	if (scriptContext.type == 'create' || scriptContext.type == 'edit')
+    	if (scriptContext.type == scriptContext.UserEventType.CREATE || scriptContext.type == scriptContext.UserEventType.EDIT)
     		{
-		    	// declare variables
-		    	var lineCurrency;
-		    	var addCurrency = true;
-		    	
 		    	// get the ID of the current record
 		    	var recordID = scriptContext.newRecord.id;
 		    	
@@ -61,85 +57,102 @@ function(record, search) {
 		    		isDynamic: true
 		    	});
 		    	
-		    	// get the value of the subsidiary field from the current record
-		    	var subsidiary = currentRecord.getValue({
-		    		fieldId: 'subsidiary'
+		    	// get count of subsdiaries
+		    	var subsidiaries = currentRecord.getLineCount({
+		    		sublistId: 'submachine'
 		    	});
 		    	
-		    	// lookup fields on the subsidiary record
-		    	var subsidiaryLookup = search.lookupFields({
-		    		type: search.Type.SUBSIDIARY,
-		    		id: subsidiary,
-		    		columns: ['currency']
-		    	});
-		    	
-		    	// retrieve the base currency from the subsidiary record
-		    	var baseCurrency = subsidiaryLookup.currency[0].value;
-		    	
-		    	// get count of lines in the 'Currencies' sublist
-		    	var lineCount = currentRecord.getLineCount({
-		    		sublistId: 'currency'
-		    	});
-		    	
-		    	// loop through lineCount
-		    	for (var i = 0; i < lineCount; i++)
+		    	// loop through subsidiaries
+		    	for (var i = 0; i < subsidiaries; i++)
 		    		{
-		    			// get the currency from the line
-		    			lineCurrency = currentRecord.getSublistValue({
-		    				sublistId: 'currency',
-		    				fieldId: 'currency',
+			    		// declare variables
+				    	var addCurrency = true;
+		    		
+		    			// get the internal ID of the subsidiary
+		    			var subsidiaryID = currentRecord.getSublistValue({
+		    				sublistId: 'submachine',
+		    				fieldId: 'subsidiary',
 		    				line: i
 		    			});
 		    			
-		    			// check if the baseCurrency and lineCurrency variables are the same
-		    			if (baseCurrency == lineCurrency)
-		    				{
-		    					// set addCurrency variable to false
-		    					addCurrency = false;
-		    					
-		    					// break the loop
-		    					break;
-		    				}
+		    			// lookup fields on the subsidiary record
+				    	var subsidiaryLookup = search.lookupFields({
+				    		type: search.Type.SUBSIDIARY,
+				    		id: subsidiaryID,
+				    		columns: ['currency']
+				    	});
+				    	
+				    	// retrieve the base currency from the subsidiary record
+				    	var baseCurrency = subsidiaryLookup.currency[0].value;
+				    	
+				    	// get count of lines in the 'Currencies' sublist
+				    	var currencies = currentRecord.getLineCount({
+				    		sublistId: 'currency'
+				    	});
+				    	
+				    	// loop through currencies
+				    	for (var x = 0; x < currencies; x++)
+				    		{
+					    		// get the currency from the line
+				    			var lineCurrency = currentRecord.getSublistValue({
+				    				sublistId: 'currency',
+				    				fieldId: 'currency',
+				    				line: i
+				    			});
+				    			
+				    			// check if the baseCurrency and lineCurrency variables are the same
+				    			if (baseCurrency == lineCurrency)
+				    				{
+				    					// set addCurrency variable to false
+				    					addCurrency = false;
+				    					
+				    					// break the loop
+				    					break;
+				    				}
+				    		}
+				    	
+				    	// check value of addCurrency variable returns true
+				    	if (addCurrency == true)
+				    		{
+				    			// select a new line in the 'Currencies' sublist
+				    			currentRecord.selectNewLine({
+				    				sublistId: 'currency'
+				    			});
+				    			
+				    			// set currency field on the new line
+				    			currentRecord.setCurrentSublistValue({
+				    				sublistId: 'currency',
+				    				fieldId: 'currency',
+				    				value: baseCurrency
+				    			});
+				    			
+				    			// commit the new line
+				    			currentRecord.commitLine({
+				    				sublistId: 'currency'
+				    			});
+				    		}
+				    	
 		    		}
 		    	
-		    	// check value of addCurrency variable returns true
-		    	if (addCurrency == true)
-		    		{
-		    			// select a new line in the 'Currencies' sublist
-		    			currentRecord.selectNewLine({
-		    				sublistId: 'currency'
-		    			});
-		    			
-		    			// set currency field on the new line
-		    			currentRecord.setCurrentSublistValue({
-		    				sublistId: 'currency',
-		    				fieldId: 'currency',
-		    				value: baseCurrency
-		    			});
-		    			
-		    			// commit the new line
-		    			currentRecord.commitLine({
-		    				sublistId: 'currency'
-		    			});
-		    			
-		    			try
-			        		{
-			        			// submit the record
-			        			currentRecord.save();
+		    	try
+			        {
+			        	// submit the record
+			        	currentRecord.save({
+			        		ignoreMandatoryFields: true
+			        	});
 			        			
-			        			log.audit({
-			        				title: 'Currency Added to Customer Record',
-			        				details: 'Record ID: ' + recordID
-			        			});
-			        		}
-			        	catch(error)
-			        		{
-			        			log.error({
-			        				title: 'Error Updating Customer Record ' + recordID,
-			        				details: error
-			        			});
-			        		}
-		    		}
+			        	log.audit({
+			        		title: 'Currency Added to Customer Record',
+			        		details: 'Record ID: ' + recordID
+			        	});
+			        }
+		    	catch(error)
+			    	{
+			        	log.error({
+			        		title: 'Error Updating Customer Record ' + recordID,
+			        		details: error
+			        	});
+			        }
     		}
     }
 
